bf220afe84a5ba84ec6068c7baf73f2fc4ec39bb
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-02-05 20:48:25 +0100

    Fix payload for push notifications

--- src/services/sendNotif.js
+++ src/services/sendNotif.js
@@ -3,7 +3,7 @@ import webpush from 'web-push';
 export default (pushSubscription, data) => 
     webpush.sendNotification(
         pushSubscription,
-        JSON.stringify(data)
+        JSON.stringify(data.comment)
     )
     .then(console.log)
     .catch(console.log);
